524e336248ebfe02099fb73f1b9e24fffaf21624
JonathanZils <jo_zils@hotmail.com>
2022-01-11 11:18:09 -0300

    Add project files.

---
+++ Core/Inquilinos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core
+{
+    public class Inquilinos
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public int Telefone { get; set; }
+
+        public EmailAddressAttribute Email { get; set; }
+
+        public SexoDoInquilino Sexo { get; set; }
+
+        public enum SexoDoInquilino
+        {
+            Masculino,
+            Feminino
+        }
+
+    }
+}
---
+++ Core/Unidades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core
+{
+    class Unidades
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Identificacao { get; set; }
+
+        public string Proprietario { get; set; }
+
+        public string Condominio { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
